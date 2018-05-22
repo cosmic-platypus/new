df6f0ec58644ce9500433c34264063f63fc83ac3
Evan Scott <evan@Evans-MacBook-Pro.local>
2018-05-21 22:47:02 -0400

    This is the legendary first version of the ultimate showdown calculator.

--- dick.py
+++ dick.py
@@ -1,1 +1,22 @@
-print("hello world")
+def dominance(s1, s2):
+    if (s1 == "goose" or s2 == "goose"):
+        return "goose"
+    elif (s1 == "Max" and s2 != "Max"):
+        return s2
+    elif (s2 == "Max" and s1 != "Max"):
+        return s1
+    elif (s1 == "Max" and s2 == "Max"):
+        return "Cuck."
+    else:
+        return "The goose rules all."
+
+competitors = [
+    ("Max", "Kat"), 
+    ("goose", "Max"),
+    ("Evan", "Max"),
+    ("Max", "Max"),
+    ("poop", "pee")
+]
+
+for couple in competitors:
+    print(dominance(*couple))
